b3f47f9f77a14abd6f30c976911db506fb611052
maddy020 <madhavsetia24@gmail.com>
2025-12-07 18:11:51 +0530

    backend: (task creation)api

--- backend/src/controller/tasks.ts
+++ backend/src/controller/tasks.ts
@@ -1,4 +1,4 @@
-import { getAllTasksFromDb } from "../model/tasks";
+import { createTaskInDb, getAllTasksFromDb } from "../model/tasks";
 import { customRequest } from "../utils/types";
 import type { Response } from "express";
 
@@ -6,10 +6,10 @@ export async function getAllTasks(req: customRequest, res: Response) {
   try {
     const user = req.user;
     if (!user) {
-      return res.status(404).json({ message: "No user Found" });
+      return res.status(401).json({ message: "No user Found" });
     }
     const response = await getAllTasksFromDb(user);
-    if (response) {
+    if (response.success) {
       return res
         .status(200)
         .json({ message: "Success", tasks: response.result });
@@ -21,7 +21,29 @@ export async function getAllTasks(req: customRequest, res: Response) {
   }
 }
 
-export async function createTask() {}
+export async function createTask(req: customRequest, res: Response) {
+  try {
+    const user = req.user;
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({ message: "No User Found" });
+    }
+    const { task } = req.body;
+    if (!(task.title || task.priority || task.status)) {
+      return res.status(404).json({ message: "Task Fields can not be empty" });
+    }
+    const response = await createTaskInDb(user, task);
+    if (response.success) {
+      return res
+        .status(200)
+        .json({ message: "Success", task: response.result });
+    }
+    return res.status(400).json({ message: "Fail", task: {} });
+  } catch (error) {
+    console.log("Error in getting tasks of the user", error);
+    return res.status(500).json({ message: "Internal Server Error", task: {} });
+  }
+}
 
 export async function updateTask() {}
 
--- backend/src/model/tasks.ts
+++ backend/src/model/tasks.ts
@@ -9,24 +9,64 @@ export async function getAllTasksFromDb(user: JwtPayload) {
     ]);
     if (allTasks) {
       return {
+        success: true,
         result: allTasks.rows,
         rowCount: allTasks.rowCount,
       };
     }
     return {
+      success: true,
       result: [],
       rowCount: 0,
     };
   } catch (error) {
     console.log("Error in get query from db", error);
     return {
+      success: false,
       result: [],
       rowCount: 0,
     };
   }
 }
 
-export async function createTaskInDb() {}
+export async function createTaskInDb(user: JwtPayload, task: any) {
+  try {
+    const userId = user.id;
+    const dbQuery = `INSERT INTO tasks
+        (user_id, title, description, due_date, priority, status)
+      VALUES
+        ($1, $2, $3, $4, $5, $6)
+      RETURNING *;`;
+    const params = [
+      userId,
+      task.title,
+      task.description || null,
+      task.due_date || null,
+      task.priority || "medium",
+      task.status || "todo",
+    ];
+    const response = await query(dbQuery, params);
+    if (response) {
+      return {
+        success: true,
+        result: response.rows,
+        rowCount: response.rowCount,
+      };
+    }
+    return {
+      success: true,
+      result: [],
+      rowCount: 0,
+    };
+  } catch (error) {
+    console.log("Error in creating the tasks in db", error);
+    return {
+      success: false,
+      result: [],
+      rowCount: 0,
+    };
+  }
+}
 
 export async function updateTaskInDb() {}
 
