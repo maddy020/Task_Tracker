97dfc067fb914f1655e8f2e47036e0659ff2f8eb
maddy020 <madhavsetia24@gmail.com>
2025-12-07 19:21:15 +0530

    backend:updateTasks, deleteTasks, searchTasks api has been written

--- backend/src/controller/tasks.ts
+++ backend/src/controller/tasks.ts
@@ -1,4 +1,10 @@
-import { createTaskInDb, getAllTasksFromDb } from "../model/tasks";
+import {
+  createTaskInDb,
+  deleteTaskInDb,
+  getAllTasksFromDb,
+  searchTaskInDb,
+  updateTaskInDb,
+} from "../model/tasks";
 import { customRequest } from "../utils/types";
 import type { Response } from "express";
 
@@ -24,7 +30,6 @@ export async function getAllTasks(req: customRequest, res: Response) {
 export async function createTask(req: customRequest, res: Response) {
   try {
     const user = req.user;
-    console.log(user);
     if (!user) {
       return res.status(401).json({ message: "No User Found" });
     }
@@ -45,8 +50,72 @@ export async function createTask(req: customRequest, res: Response) {
   }
 }
 
-export async function updateTask() {}
+export async function updateTask(req: customRequest, res: Response) {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ message: "No User Found" });
+    }
+    const { taskId } = req.params;
+    const { taskDetails } = req.body;
+    const parsedId = parseInt(taskId as string);
+    if (!parsedId || !taskDetails) {
+      return res.status(404).json({ message: "task details can not be empty" });
+    }
+    const response = await updateTaskInDb(user, parsedId, taskDetails);
+    if (response.success) {
+      return res.status(200).json({ message: response.message });
+    }
+    return res.status(400).json({ message: response.message });
+  } catch (error) {
+    console.log("Error in getting tasks of the user", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
 
-export async function deleteTask() {}
+export async function deleteTask(req: customRequest, res: Response) {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ message: "No User Found" });
+    }
+    const { taskId } = req.params;
+    const parsedId = parseInt(taskId as string);
+    if (!parsedId) {
+      return res.status(404).json({ message: "task id can not be empty" });
+    }
+    const response = await deleteTaskInDb(user, parsedId);
+    if (response.success) {
+      return res.status(200).json({ message: "Success" });
+    }
+    return res.status(400).json({ message: "Fail" });
+  } catch (error) {
+    console.log("Error in getting tasks of the user", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
 
-export async function searchTask() {}
+export async function searchTask(req: customRequest, res: Response) {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ message: "No User Found" });
+    }
+    const { searchKeyword } = req.query;
+    if (!searchKeyword) {
+      return res.status(404).json({ message: "No keyword found" });
+    }
+    const response = await searchTaskInDb(user, searchKeyword as string);
+    if (response.success) {
+      return res
+        .status(200)
+        .json({ message: "Success", tasks: response.result });
+    }
+    return res.status(400).json({ message: "Fail", tasks: [] });
+  } catch (error) {
+    console.log("Error in getting tasks of the user", error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", tasks: [] });
+  }
+}
--- backend/src/model/tasks.ts
+++ backend/src/model/tasks.ts
@@ -4,7 +4,7 @@ import { query } from "../utils/db";
 export async function getAllTasksFromDb(user: JwtPayload) {
   try {
     const userId = user.id;
-    const allTasks = await query("SELECT * FROM tasks where user_id=${1}", [
+    const allTasks = await query(`SELECT * FROM tasks where user_id=$1`, [
       userId,
     ]);
     if (allTasks) {
@@ -68,8 +68,135 @@ export async function createTaskInDb(user: JwtPayload, task: any) {
   }
 }
 
-export async function updateTaskInDb() {}
+export async function updateTaskInDb(
+  user: JwtPayload,
+  taskId: number,
+  taskDetails: any
+) {
+  try {
+    const userId = user.id;
+    let index = 1;
 
-export async function deleteTaskInDb() {}
+    const setClauses: string[] = [];
+    const params: any[] = [];
 
-export async function searchTaskInDb() {}
+    const isExistingTask = await query(`SELECT * FROM tasks where id=$1`, [
+      taskId,
+    ]);
+    if (isExistingTask.rowCount == 0) {
+      return {
+        success: true,
+        message: "No such task found",
+      };
+    }
+    if (taskDetails.title) {
+      setClauses.push(`title = $${index++}`);
+      params.push(taskDetails.title);
+    }
+
+    if (taskDetails.description) {
+      setClauses.push(`description = $${index++}`);
+      params.push(taskDetails.description);
+    }
+
+    if (taskDetails.status) {
+      setClauses.push(`status = $${index++}`);
+      params.push(taskDetails.status);
+    }
+
+    if (taskDetails.priority) {
+      setClauses.push(`priority = $${index++}`);
+      params.push(taskDetails.priority);
+    }
+
+    if (taskDetails.due_date) {
+      setClauses.push(`due_date = $${index++}`);
+      params.push(taskDetails.due_date);
+    }
+    if (setClauses.length === 0) {
+      return {
+        success: false,
+        message: "No valid fields to update",
+      };
+    }
+
+    const dbQuery = `
+      UPDATE tasks
+      SET ${setClauses.join(", ")}
+      WHERE id = $${index} AND user_id = $${index + 1}
+      RETURNING *;
+    `;
+
+    params.push(taskId, userId);
+
+    const response = await query(dbQuery, params);
+
+    return {
+      success: true,
+      message: "Success",
+      result: response.rows,
+    };
+  } catch (error) {
+    console.log("Error in updating the task in db", error);
+    return {
+      success: false,
+      message: "Fail",
+      result: [],
+    };
+  }
+}
+
+export async function deleteTaskInDb(user: JwtPayload, taskId: Number) {
+  try {
+    const userId = user.id;
+    const response = await query(
+      `DELETE FROM tasks where id=$1 and user_id=$2`,
+      [taskId, userId]
+    );
+    if (response) {
+      return {
+        success: true,
+        result: response.rows,
+      };
+    }
+    return {
+      success: true,
+      result: [],
+    };
+  } catch (error) {
+    console.log("Error in deleting  the task in db", error);
+    return {
+      success: false,
+      result: [],
+    };
+  }
+}
+
+export async function searchTaskInDb(user: JwtPayload, searchKeyword: string) {
+  try {
+    const userId = user.id;
+    const response = await query(
+      `SELECT * FROM tasks
+   WHERE (title ILIKE '%' || $1 || '%' 
+      OR description ILIKE '%' || $1 || '%')
+     AND user_id = $2`,
+      [searchKeyword, userId]
+    );
+    if (response) {
+      return {
+        success: true,
+        result: response.rows,
+      };
+    }
+    return {
+      success: true,
+      result: [],
+    };
+  } catch (error) {
+    console.log("Error in deleting  the task in db", error);
+    return {
+      success: false,
+      result: [],
+    };
+  }
+}
